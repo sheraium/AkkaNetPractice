5e0a713a47b9543784a475eac39a5205cb77a1d3
Peter <sheraium@gmail.com>
2020-07-11 15:23:21 +0800

    Lesson 1.2: Defining and Handling Messages

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Akka.Actor;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public static ActorSystem MyActorSystem;
+
+        private static void Main(string[] args)
+        {
+            // make an actor system
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
+
+            // make our first actors!
+            IActorRef consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()),
+                "consoleWriterActor");
+            IActorRef consoleReaderActor =
+                MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)),
+                    "consoleReaderActor");
+
+            // tell console reader to begin
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+
+            // blocks the main thread from exiting until the actor system is shut down
+            MyActorSystem.WhenTerminated.Wait();
+        }
+    }
+
+    internal class MyActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            if (message is Messages.InputError)
+            {
+                var msg = message as Messages.InputError;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(msg.Reason);
+            }
+            else
+            {
+                Unhandled(message);
+            }
+        }
+    }
+}
