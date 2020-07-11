1a5198250a8ab6e639d9387910dc47bddedae37d
Peter <sheraium@gmail.com>
2020-07-11 14:52:46 +0800

    Lesson 1.1: Actors and the ActorSystem

--- ConsoleApp1/Messages/StopMovieMessage.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ConsoleApp1.Messages
-{
-    public class StopMovieMessage
-    {
-    }
-}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,33 +1,104 @@
 ﻿using System;
 using Akka.Actor;
-using ConsoleApp1.Actors;
-using ConsoleApp1.Messages;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
+        public static ActorSystem MyActorSystem;
+
         private static void Main(string[] args)
         {
-            var actorSystem = ActorSystem.Create("MovieStreamingActorSystem");
-            Console.WriteLine("Actor system Created");
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            //var playbackActorProps = Props.Create<PlaybackActor>();
-            //IActorRef playbackActorRef = actorSystem.ActorOf(playbackActorProps, "PlaybackActor");
-            var userActorProps = Props.Create<UserActor>();
-            var userActorRef = actorSystem.ActorOf(userActorProps, "UserActor");
+            PrintInstructions();
 
-            userActorRef.Tell(new PlayMovieMessage("Codenan the Destroyer", 42));
+            // make our first actors!
+            IActorRef consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()),
+                "consoleWriterActor");
+            IActorRef consoleReaderActor =
+                MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)),
+                    "consoleReaderActor");
 
-            userActorRef.Tell(new PlayMovieMessage("Boolean Lies", 42));
+            // tell console reader to begin
+            consoleReaderActor.Tell("start");
 
-            userActorRef.Tell(new StopMovieMessage());
+            // blocks the main thread from exiting until the actor system is shut down
+            MyActorSystem.WhenTerminated.Wait();
+        }
 
-            userActorRef.Tell(new StopMovieMessage());
+        private static void PrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.Write("Some lines will appear as");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(" red ");
+            Console.ResetColor();
+            Console.Write(" and others will appear as");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" green! ");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+    }
 
-            Console.ReadLine();
+    internal class ConsoleReaderActor : UntypedActor
+    {
+        public const string ExitCommand = "exit";
+        private IActorRef _consoleWriterActor;
 
-            actorSystem.Dispose();
+        public ConsoleReaderActor(IActorRef consoleWriterActor)
+        {
+            _consoleWriterActor = consoleWriterActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            var read = Console.ReadLine();
+            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                // shut down the system (acquire handle to system via
+                // this actors context)
+                Context.System.Terminate();
+                return;
+            }
+
+            // send input to the console writer to process and print
+            _consoleWriterActor.Tell(read);
+
+            // continue reading messages from the console
+            Self.Tell("continue");
+        }
+    }
+
+    /// <summary>
+    /// Actor responsible for serializing message writes to the console.
+    /// (write one message at a time, champ :)
+    /// </summary>
+    internal class ConsoleWriterActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            var msg = message as string;
+
+            // make sure we got a message
+            if (string.IsNullOrEmpty(msg))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Please provide an input.\n");
+                Console.ResetColor();
+                return;
+            }
+
+            // if message has even # characters, display in red; else, green
+            var even = msg.Length % 2 == 0;
+            var color = even ? ConsoleColor.Red : ConsoleColor.Green;
+            var alert = even ? "Your string had an even # of characters.\n" : "Your string had an odd # of characters.\n";
+            Console.ForegroundColor = color;
+            Console.WriteLine(alert);
+            Console.ResetColor();
         }
     }
 }
