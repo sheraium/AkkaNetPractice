dc0c371ff9996aa1a496058be751453c2b5090da
peter <sheraium@gmail.com>
2020-10-18 17:02:28 +0800

    finish Lesson 1.3

---
+++ Lesson1_3_PropsAndIActorRefs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Akka.Actor;
+
+namespace Lesson1_3_PropsAndIActorRefs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var actorSystem = ActorSystem.Create("MyActorSystem");
+
+            var consoleWriterProps = Props.Create<ConsoleWriterActor>();
+            var consoleWriterActor = actorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
+
+            var validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
+            var validationActor = actorSystem.ActorOf(validationActorProps, "validationActor");
+
+            var consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
+            var consoleReaderActor = actorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
+
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+
+            actorSystem.WhenTerminated.Wait();
+        }
+    }
+}
