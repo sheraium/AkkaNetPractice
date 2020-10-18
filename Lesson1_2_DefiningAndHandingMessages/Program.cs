21a4e27f3b07a5e2ada48ca4e6e1d15dae6f352b
peter <sheraium@gmail.com>
2020-10-18 14:21:57 +0800

    finish Lesson 1.2

---
+++ Lesson1_2_DefiningAndHandingMessages/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Akka.Actor;
+
+namespace Lesson1_2_DefiningAndHandingMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var actorSystem = ActorSystem.Create("MyActorSystem");
+
+            var consoleWriterActor = actorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
+            var consoleReaderActor = actorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
+           
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+
+            actorSystem.WhenTerminated.Wait();
+
+        }
+    }
+}
